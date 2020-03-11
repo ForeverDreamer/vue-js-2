e73c4039c15b68f912af9d7aea6ff457b5437ab5
doer <499361328@qq.com>
2020-03-11 20:18:10 +0800

    frontend: 08 Communicating between Components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,36 +3,53 @@
 <!--    <h1>全局组件</h1>-->
 <!--    <app-servers></app-servers>-->
 <!--    <hr>-->
+
+<!--    <div class="container">-->
+<!--      <app-header></app-header>-->
+<!--      <hr>-->
+<!--      <div class="row">-->
+<!--        <servers></servers>-->
+<!--        <app-server-details></app-server-details>-->
+<!--      </div>-->
+<!--      <hr>-->
+<!--      <app-footer></app-footer>-->
+<!--    </div>-->
+
     <div class="container">
-      <app-header></app-header>
-      <hr>
       <div class="row">
-        <servers></servers>
-        <app-server-details></app-server-details>
+        <div class="col-xs-12">
+          <app-user></app-user>
+        </div>
       </div>
-      <hr>
-      <app-footer></app-footer>
     </div>
+
   </div>
 </template>
 
 <script>
   //局部组件
-  import Header from './components/shared/Header';
-  import Servers from './components/server/Servers';
-  import ServerDetails from  './components/server/ServerDetails';
-  import Footer from './components/shared/Footer';
+  // import Header from './components/shared/Header';
+  // import Servers from './components/server/Servers';
+  // import ServerDetails from  './components/server/ServerDetails';
+  // import Footer from './components/shared/Footer';
+
+  import User from './components/user/User.vue';
 
   export default {
     name: 'App',
     components: {
-      appHeader: Header,
-      Servers,
-      'app-server-details': ServerDetails,
-      'app-footer': Footer
+      // appHeader: Header,
+      // Servers,
+      // 'app-server-details': ServerDetails,
+      // 'app-footer': Footer,
+      appUser: User
     }
   }
 </script>
 
 <style>
+  div.component {
+    border: 1px solid black;
+    padding: 30px;
+  }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,16 @@ Vue.config.productionTip = false;
 // 全局组件
 // Vue.component('app-servers', Home);
 
+export const eventBus = new Vue({
+  methods: {
+    changeAge(age) {
+      this.$emit('ageWasEdited', age);
+    }
+  }
+});
+
 new Vue({
   render: h => h(App),
 }).$mount('#app');
+
+
